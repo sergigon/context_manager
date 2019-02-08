2ddc07badd48d396b290870c4149cd2247b788b2
MINI ROBOT <mini.uc3m@gmail.com>
2019-02-08 09:15:58 +0100

    fixed exceptions

--- src/context_manager/pc-tablet_conection.py
+++ src/context_manager/pc-tablet_conection.py
@@ -53,12 +53,14 @@ def handler(signum, frame):
 def progress(filename, size, sent):
     # Global variables
     global filename_prev, sent_prev
-    sys.stdout.write("%s\'s progress: %.2f%%   \r" % (filename, float(sent)/float(size)*10) )
-    #print("%s\'s progress: %.2f%%   \r" % (filename, float(sent)/float(size)*10) )
+    #sys.stdout.write("%s\'s progress: %.2f%%   \r" % (filename, float(sent)/float(size)*10) )
+    print("%s\'s progress: %.2f%%   \r" % (filename, float(sent)/float(size)*10) )
     # Checks if it is stuck
-    if(filename_prev!=filename and sent_prev!=sent): # It is not stuck
+    if(filename_prev!=filename or sent_prev!=sent): # It is not stuck
         # Restart timer
-        signal.alarm(5)
+        print('restart')
+        signal.alarm(10)
+        
     # Fill prev variables
     filename_prev = filename
     sent_prev = sent
@@ -72,60 +74,73 @@ def createSSHClient(server, port, user, password):
 
 def transferTablet2PC(remote_path, local_path, recursive = True):
     print('Tablet -> PC')
-    # Establish connection
+    # Register the signal function handler
+    signal.signal(signal.SIGALRM, handler)
+    # Establish ssh connection
     try:
+        print('Set alarm')
+        signal.alarm(5) # Start the alarm
         # Create ssh object
         print('Create ssh object')
         ssh = createSSHClient('10.42.0.34', 8022, 'u0_a156', key)
-        # Create scp object
-        print('Create scp object')
-        scp = SCPClient(ssh.get_transport(), progress=progress)
-    except paramiko.ssh_exception.NoValidConnectionsError as e:
-        rospy.logerr('NoValidConnectionsError: %s ' % e)
-        print ('NoValidConnectionsError: %s ' % e)
-        return -1
     except socket.error as e:
         rospy.logerr('%s' % e)
         print e
         return -1
-    except paramiko.ssh_exception.SSHException as e:
-        rospy.logerr('%s' % e)
-        print e
+    except paramiko.ssh_exception.NoValidConnectionsError as e:
+        rospy.logerr('NoValidConnectionsError: %s ' % e)
+        print ('NoValidConnectionsError: %s ' % e)
         return -1
-    # Transfer files
-    try:
-        print('Transfering files')
-        global filename_prev, sent_prev
-        filename_prev, sent_prev = '', 0
-        print('Set alarm')
-        # Register the signal function handler
-        signal.signal(signal.SIGALRM, handler)
-        signal.alarm(5) # Start the alarm
-        # Get method
-        scp.get(remote_path, local_path, recursive = True)
-        signal.alarm(0) # Disable the alarm
-    except SCPException as e: # No tablet file
-        rospy.logerr('SCPException: %s ' % e)
-        print ('SCPException: %s ' % e)
-    except IOError as e: # No PC directory
-        rospy.logerr('IOError: %s ' % e)
-        print ('IOError: %s ' % e)
+    except paramiko.ssh_exception.SSHException as e:
+        rospy.logerr('SSHException: %s' % e)
+        print ('SSHException: %s' % e)
+        return -1
     except TimeOut as e: # TimeOut transfer
         rospy.logerr('TimeOut: %s ' % e)
         print ('TimeOut: %s ' % e)
+        return -1
+    # Disable the alarm
+    signal.alarm(0)
+    # Transfer files
+    result = 0
+    try:
+        # Create scp object
+        print('Create scp object')
+        scp = SCPClient(ssh.get_transport(), progress=progress)
+        global filename_prev, sent_prev
+        filename_prev, sent_prev = '', 0
+        print('Set alarm')
+        signal.alarm(5) # Start the alarm
+        # Get method
+        print('Transfering files: %s -> %s' %(remote_path, local_path))
+        scp.get(remote_path, local_path, recursive = True)
+    except SCPException as e: # No tablet file
+        rospy.logerr('SCPException: %s ' % e)
+        print ('SCPException: %s ' % e)
+        result = -1
+    except IOError as e: # No PC directory
+        rospy.logerr('IOError: %s ' % e)
+        print ('IOError: %s ' % e)
+        result = -1
+    except TimeOut as e: # TimeOut transfer
+        rospy.logerr('TimeOut: %s ' % e)
+        print ('TimeOut: %s ' % e)
+        result = -1
+    # Disable the alarm
+    signal.alarm(0)
     # Close scp object
     scp.close()
+    return result
 
 def transferPC2Tablet(local_path, remote_path, recursive = True):
     print('PC -> Tablet')
     # Establish connection
     try:
+        print('Set alarm')
+        signal.alarm(5) # Start the alarm
         # Create ssh object
         print('Create ssh object')
         ssh = createSSHClient('10.42.0.34', 8022, 'u0_a156', key)
-        # Create scp object
-        print('Create scp object')
-        scp = SCPClient(ssh.get_transport(), progress=progress)
     except paramiko.ssh_exception.NoValidConnectionsError as e:
         rospy.logerr('NoValidConnectionsError: %s ' % e)
         print ('NoValidConnectionsError: %s ' % e)
@@ -138,28 +153,46 @@ def transferPC2Tablet(local_path, remote_path, recursive = True):
         rospy.logerr('%s' % e)
         print e
         return -1
+    # Disable the alarm
+    signal.alarm(0)
     # Transfer files
+    result = 0
     try:
-        print('Transfering files')
+        # Create scp object
+        print('Create scp object')
+        scp = SCPClient(ssh.get_transport(), progress=progress)
         global filename_prev, sent_prev
         filename_prev, sent_prev = '', 0
         print('Set alarm')
         # Register the signal function handler
         signal.signal(signal.SIGALRM, handler)
-        signal.alarm(5) # Start the alarm
+        #signal.alarm(5) # Start the alarm
         # Put method
+        print('Transfering files: %s -> %s' %(local_path, remote_path))
         print scp.put(local_path, recursive=recursive, remote_path=remote_path)
-        signal.alarm(0) # Disable the alarm
+        # Close scp object
+        print('Closing scp')
+        scp.close()
     except OSError as e: # No PC file
         rospy.logerr('%s' % e)
+        result = -1
     except SCPException as e: # No tablet directory
         rospy.logerr('%s' % e)
         print e
+        result = -1
     except TimeOut as e: # TimeOut transfer
         rospy.logerr('TimeOut: %s ' % e)
         print ('TimeOut: %s ' % e)
-    # Close scp object
-    scp.close()
+        result = -1
+    except EOFError as e: # TimeOut transfer
+        rospy.logerr('EOFError: %s ' % e)
+        print ('EOFError: %s ' % e)
+        result = -1
+    print('Disable alarm')
+    # Disable the alarm
+    signal.alarm(0)
+    print('CReturn')
+    return result
 
 def getPassword():
     pass
@@ -171,15 +204,13 @@ def loop_forever():
         print "sec"
         time.sleep(1)
 
-
-
-
 # Main
 if __name__ == '__main__':
     print('hola\r')
     print('aaaaa\r')
-    transferTablet2PC('/sdcard/multimedia/image/weather/wikipedia/', '/home/user/Documentos/')
-
+    #transferTablet2PC('/sdcard/multimedia/image/weather/wikipedia/', '/home/user/Documentos/')
+    transferPC2Tablet('/home/user/Campeones.mp4', remote_path='/sdcard/multimedia/')
+    transferPC2Tablet('/home/user/ROS/catkin_dev/src/time_weather_skill/data/weather_icons/wikipedia', remote_path='/sdcard/multimedia/')
     loop_forever()
 
     try:
